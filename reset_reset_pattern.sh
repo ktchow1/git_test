4417d85bb5497edbe108622de1cc64ba3a44b120
ktchow1 <ktchow1@yahoo.com.hk>
2021-04-11 13:32:57 +0800

    reset

--- reset_reset_pattern.sh
+++ reset_reset_pattern.sh
@@ -27,13 +27,14 @@ echo AAA > A.txt; git add A.txt; git commit -m "add A";
 echo BBB > B.txt; git add B.txt; git commit -m "add B"; 
 echo CCC > C.txt; git add C.txt; git commit -m "add C"; git branch dev
 git push -u origin master
+echo XXX > A.txt; git add A.txt; git commit -m "XXX A";
 echo DDD > D.txt; git add D.txt; git commit -m "add D"; git branch temp 
 echo EEE > E.txt; git add E.txt; git commit -m "add E"; 
 echo FFF > F.txt; git add F.txt; git commit -m "add F"; 
 git push 
 
 git checkout dev
-rm A.txt;         git add A.txt; git commit -m "rm A";
+echo YYY > A.txt; git add A.txt; git commit -m "YYY A";
 echo GGG > G.txt; git add G.txt; git commit -m "add G"; 
 echo HHH > H.txt; git add H.txt; git commit -m "add H"; 
 echo III > I.txt; git add I.txt; git commit -m "add I"; 
@@ -47,12 +48,12 @@ echo ""
 ##########################################
 ### [3] Dev switches to another branch ###
 ##########################################
-echo ""; echo "****** Before reset-reset ******"; ls; echo ""
+echo ""; echo "****** Before reset-reset ******"; ls; echo ""; cat A.txt
 git checkout master; 
 git reset --hard temp 
 git reset --soft dev  
 git commit -m "append temp to dev"
-echo ""; echo "****** After reset-reset ******"; ls; echo ""
+echo ""; echo "****** After reset-reset ******"; ls; echo ""; cat A.txt
 
 echo ""
 git log --oneline --decorate --all --graph
